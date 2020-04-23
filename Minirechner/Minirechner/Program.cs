904e0abf465acbf30837245fa6fc92deee5908fe
Sim410on <02fahsim@rgtfo-me.it>
2020-04-23 11:12:39 +0200

    Zahlen Einlesen

---
+++ Minirechner/Minirechner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minirechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Erste Zahl: ");
+            int ErsteZahl = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Zweite Zahl: ");
+            int ZweiteZahl = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+                
+        }
+    }
+}
